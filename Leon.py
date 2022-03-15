2d4f24b1a94ce440bf8a9aa64b29a91b4c10cecb
JuanDigital <94965256+JuanDigital@users.noreply.github.com>
2022-03-15 07:11:52 -0600

    Elegante actualización de claves

--- Leon.py
+++ Leon.py
@@ -1,82 +1,46 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb  2 14:59:37 2022
-
-@author: Juan Carlos Rodríguez Pérez
-"""
-
 import pyodbc
 import pandas as pd
-i=0
-j=0
+import openpyxl
 
-#server = 'DESKTOP-92H6KQK\SQLEXPRESS'
-#bd = 'C:\MyBusinessDatabase\MyBusinessPOS2011.mdf'
-#usuario='sa'
-#ºcontraseña='12345678'
 
-#realizar conexion a BBDD
+servidor= input('Introduce el nombre del servidor: ')
+
+server = servidor+'\SQLEXPRESS'
+database = 'C:\MyBusinessDatabase\MyBusinessPOS2011.mdf' 
+username = 'sa' 
+password = '12345678'
+
 
 try:
-    
-    conexion=pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-92H6KQK\SQLEXPRESS;DATABASE=C:\MyBusinessDatabase\MyBusinessPOS2011.mdf;UID=sa;PWD=12345678')
-    #print ('conexión exitosa!!!')
-    cursor=conexion.cursor()
-    #cursor.execute('SELECT *from prods;')
-    #row=cursor.fetchone()
-    #print(type(row))
-except Exception as FueraDeLinea:
-    print(FueraDeLinea)
-    
- #lectura de archivo excel   
+    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+    print('conexion exitosa')
+
+except:
+    print('no conecto')
+
 try:
     
     dfup=pd.read_excel('ARTICULOS ROTACION 2.xlsx')
+    print('lectura de excel Ok')
     
 except Exception as FaltaXLSX:
     print(FaltaXLSX)
-#-----------------------------se importan la columna Articulo de prods en un df
-#query = "SELECT [Articulo], [Descrip] FROM Prods;"
-query = "SELECT [Articulo] FROM Prods;"
-dfArti = pd.read_sql(query, conexion)
-#print(dfArti.head)
-'''
-#------------------------------------------------------eliminar tabla clavesadd
-cursor.execute("DELETE FROM Clavesadd") 
 
-#---------------------------------------------Insert Dataframe into SQL Server:
-for index, row in dfArti.iterrows():
-     cursor.execute("INSERT INTO Clavesadd (Clave,Dato1,Usuario,usuFecha,usuHora,Dato2,Articulo,Unidad,Imagen) VALUES(?,0,'Sys','2022-02-07 00:00:00.000','13:00:00',0,0,0,0)", row.Articulo)
-'''
-#-----------------------------------------comparar Articulo de prods con viejo
-for i in range(0,1377):
+#-----------------------------se importa la columna Articulo de clavesadd en un df
+query = "SELECT [Articulo] FROM clavesadd;"
+dfArti = pd.read_sql(query, cnxn)
+#-----------------------------------------comparar Articulo de clavesadd con viejo
+for i in range(0,12):
     clave=str(dfArti.loc[i,'Articulo'])
     for j in range(0,1201):
         ArtiV=str(dfup.loc[j,'Viejo'])
         if clave==ArtiV:
             ArtiN=dfup.loc[j,'Nuevo']
-            print(i,j,ArtiN) #---------------------------------actualizar prods
-            cursor.execute("UPDATE prods SET ARTICULO='"+ArtiN+"' WHERE ARTICULO='"+ArtiV+"'")
-        #else:
-         #   print()
-'''    
-clave=str(dfArti.loc[219,'Articulo'])            
-ArtiV=str(dfup.loc[0,'Viejo'])
-ArtiN=str(dfup.loc[0,'Nuevo'])
+            print(i,j,ArtiN,ArtiV) #---------------------------------actualizar prods
+            cursor.execute("UPDATE Clavesadd SET Clave='"+ArtiN+"' WHERE ARTICULO='"+ArtiV+"'")
 
 
-print(clave,ArtiV)
-if clave<=ArtiV:
-    print('ok')
-else: print('nok')    
-    #ArtiN=dfup.loc[i,'Nuevo']
-    #print(ArtiV,ArtiN)
-
- 
-
-print("UPDATE prods SET ARTICULO='"+ArtiN+"' WHERE ARTICULO='"+ArtiV+"'")
-cursor.execute("UPDATE prods SET ARTICULO='"+ArtiN+"' WHERE ARTICULO='"+ArtiV+"'")
-'''
-
-conexion.commit()
+        
+cnxn.commit()
 cursor.close()
